a5108798dc8f78250ed73178e7e4957e48794f18
Achim <git@achimonline.de>
2020-08-31 17:52:36 +0200

    upgrade to Electron 10; updated dependencies; removal of "electron-window-state"

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,5 @@
-'use strict';
-
-const electron = require('electron');
-const store = require('electron-store');
-const windowStateKeeper = require('electron-window-state');
-
-const app = electron.app;
-const dialog = electron.dialog;
-const menu = electron.Menu;
-
+const {app, dialog, Menu, BrowserWindow, nativeImage} = require('electron');
+const store = new (require('electron-store'))();
 const index = __dirname + '/../index.html';
 
 const menuTemplate = [
@@ -95,7 +87,7 @@ const menuTemplate = [
 
 					let callback = function(buttonIndex) {
 						if (buttonIndex == 0) {
-							(new store()).clear();
+							store.clear();
 							mainWindow.loadFile(index);
 						}
 					};
@@ -192,6 +184,52 @@ require('electron-debug')({
 // prevent window being garbage collected
 let mainWindow;
 
+function windowStateKeeper(windowName) {
+	let window;
+
+	// default values
+	let windowState = {
+		x: undefined,
+		y: undefined,
+		width: 1024,
+		height: 768
+	};
+
+	function setBounds() {
+		if (store.has(`windowState.${windowName}`)) {
+			windowState = store.get(`windowState.${windowName}`);
+		}
+	}
+
+	function saveState() {
+		if (!windowState.isMaximized) {
+			windowState = window.getBounds();
+		}
+		windowState.isMaximized = window.isMaximized();
+
+		store.set(`windowState.${windowName}`, windowState);
+	}
+
+	function track(win) {
+		window = win;
+
+		['resize', 'move', 'close'].forEach(event => {
+			win.on(event, saveState);
+		});
+	}
+
+	setBounds();
+
+	return ({
+		x: windowState.x,
+		y: windowState.y,
+		width: windowState.width,
+		height: windowState.height,
+		isMaximized: windowState.isMaximized,
+		track
+	});
+}
+
 function onClosed() {
 	// dereference the window
 	// for multiple windows store them in an array
@@ -199,26 +237,23 @@ function onClosed() {
 }
 
 function createMainWindow() {
-	let winState = windowStateKeeper({
-		defaultWidth: 1000,
-		defaultHeight: 800
-	});
+	const mainWindowStateKeeper = windowStateKeeper('main');
 
-	const win = new electron.BrowserWindow({
+	const win = new BrowserWindow({
 		webPreferences: {
-			nodeIntegration: true
+			nodeIntegration: true,
+			enableRemoteModule: true
 		},
-		icon: electron.nativeImage.createFromPath(__dirname + '/../img/icon.png'),
-		x: winState.x,
-		y: winState.y,
-		width: winState.width,
-		height: winState.height
+		icon: nativeImage.createFromPath(__dirname + '/../img/icon.png'),
+		x: mainWindowStateKeeper.x,
+		y: mainWindowStateKeeper.y,
+		width: mainWindowStateKeeper.width,
+		height: mainWindowStateKeeper.height
 	});
-
-	winState.manage(win);
-
     win.loadFile(index);
 	win.on('closed', onClosed);
+
+	mainWindowStateKeeper.track(win);
 
 	return win;
 }
@@ -236,8 +271,8 @@ app.on('activate', () => {
 });
 
 app.on('ready', () => {
-	let builtMenu = menu.buildFromTemplate(menuTemplate);
-	menu.setApplicationMenu(builtMenu);
+	let builtMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(builtMenu);
 
 	mainWindow = createMainWindow();
 });
