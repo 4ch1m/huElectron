a44ba1f7048103b3e5dd583f8065a0a8f929884c
Achim <git@achimonline.de>
2023-01-06 13:27:27 +0100

    "delete user" functionality removed (no longer possible via API)

--- app/js/users.js
+++ app/js/users.js
@@ -7,21 +7,7 @@ let huE_users = {
 
 		huE_common.huejayClient.users.getAll()
 			.then(users => {
-				let templateContext = { users: users,
-                    					usedUser: huE_common.store.get(STORE_KEYS.userName) };
-
-				huE_common.setTemplate(usersTabContent, 'hbs/users.hbs', templateContext);
-			});
-	},
-
-	deleteByName(name) {
-		huE_common.huejayClient.users.delete(name)
-			.then(() => {
-				let onHiddenCallback = function() { huE_users.get() };
-				huE_common.showSuccess($.i18n('success-label-general'), $.i18n('success-msg-user-settings'), onHiddenCallback);
-			})
-			.catch(error => {
-				huE_common.showError($.i18n('error-label-general'), error.message);
+				huE_common.setTemplate(usersTabContent, 'hbs/users.hbs', {users: users});
 			});
 	}
 
